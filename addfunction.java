98b58edcb5c0884a1c27effdd266e22eda44cd3f
maha <pratyushsipu81@hmail.com>
2023-04-20 18:30:15 +0530

    my work

--- addfunction.java
+++ addfunction.java
@@ -1,3 +1,11 @@
 this is my add function
+
 5th day java lines of code
+=======
+1st day java lines of code
+2nd day java lines of code
+3rd day java lines of code
+4th day java lines of code
+6th day java lines of code
+
 
